722c19af39a6a58a3dfee6bff3daab6a5e3e3b9e
Prajwol Gyawali <prajwol.lucifer@gmail.com>
2022-10-20 13:36:48 +0545

    Use hardhat.config.ts to fix contract size issue

---
+++ hardhat.config.ts
@@ -0,0 +1,31 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "hardhat-contract-sizer";
+
+const config: HardhatUserConfig = {
+    solidity: {
+        version: "0.8.16",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+    },
+    networks: {
+        hardhat: {
+            allowUnlimitedContractSize: true,
+        },
+    },
+    contractSizer: {
+        alphaSort: true,
+        disambiguatePaths: false,
+        runOnCompile: true,
+        strict: true,
+    },
+};
+
+export default config;
